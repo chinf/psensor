19bce9145ecc83fecd671c0883584682dc378acb
Jean-Philippe Orsini <jeanfi@gmail.com>
2011-11-07 22:34:25 +0000

    code style

--- src/plib/url.c
+++ src/plib/url.c
@@ -1,27 +1,26 @@
 /*
-    Copyright (C) 2010 jeanfi@gmail.com
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-    02110-1301 USA
-*/
+ * Copyright (C) 2010-2011 jeanfi@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
 
 /*
-  Part of the following code is based on:
-  http://www.geekhideout.com/urlcode.shtml
-*/
-
+ * Part of the following code is based on:
+ * http://www.geekhideout.com/urlcode.shtml
+ */
 #include "plib/url.h"
 
 #include <ctype.h>
@@ -39,15 +38,15 @@ char *url_normalize(const char *url)
 	return ret;
 }
 
-char to_hex(char code)
+static char to_hex(char code)
 {
 	static char hex[] = "0123456789abcdef";
 	return hex[code & 0x0f];
 }
 
 /*
-   Returns a url-encoded version of str.
-*/
+ * Returns a url-encoded version of str.
+ */
 char *url_encode(const char *str)
 {
 	char *c, *buf, *pbuf;
