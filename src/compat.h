45c8086f97097df36ba8cd0552f152be80f79b12
Jean-Philippe Orsini <jeanfi@gmail.com>
2011-11-13 15:31:28 +0000

    style

--- src/compat.h
+++ src/compat.h
@@ -1,26 +1,26 @@
 /*
-    Copyright (C) 2010-2011 jeanfi@gmail.com
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-    02110-1301 USA
-*/
+ * Copyright (C) 2010-2011 jeanfi@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
 
 /*
-  gtk_dialog_get_content_area exists since gtk 2.14 As debian 5.0.6
-   (lenny) is using gtk 2.12 defines the function by using a macro.
-*/
+ * gtk_dialog_get_content_area exists since gtk 2.14 As debian 5.0.6
+ * (lenny) is using gtk 2.12 defines the function by using a macro.
+ */
 #if !defined(gtk_dialog_get_content_area)
 #define gtk_dialog_get_content_area(dialog) (GTK_DIALOG(dialog)->vbox)
 #endif
