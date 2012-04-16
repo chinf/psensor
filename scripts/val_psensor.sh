b0960738fe969a4dd1555d363ac6122173dbebcd
Jean-Philippe Orsini <jeanfi@gmail.com>
2012-04-16 09:05:37 +0000

    added gtk supressions files from GNOME.LIVE

--- scripts/val_psensor.sh
+++ scripts/val_psensor.sh
@@ -1,8 +1,15 @@
-#/bin/sh
+#/bin/bash
+
+SDIR=`dirname $0`
 
 export G_SLICE=always-malloc
 export G_DEBUG=gc-friendly 
 
 DUMP_FILE=psensor_`date +"%Y_%m_%d_%H_%M_%S"`.log
 
-valgrind --tool=memcheck --leak-check=full --leak-resolution=high --num-callers=20 --log-file=$DUMP_FILE `dirname $0`/../src/psensor
+for i in $SDIR/*supp
+do
+    SUPP="$SUPP --suppressions=$i "
+done
+
+valgrind -v $SUPP --tool=memcheck --leak-check=full --track-origins=yes --leak-resolution=high --num-callers=20 --log-file=$DUMP_FILE $SDIR/../src/psensor
