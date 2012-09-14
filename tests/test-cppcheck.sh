37010485f93021ae142fcf438810e4c16bd7ff4a
Jean-Philippe Orsini <jeanfi@gmail.com>
2012-09-14 08:34:41 +0000

    fixed cppcheck test not find config.h

--- tests/test-cppcheck.sh
+++ tests/test-cppcheck.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 EIDS=performance,portability,information,unusedFunction,missingInclude
-ROOTDIR=$srcdir/..
+ROOTDIR="$srcdir/.."
+INCLUDE_OPTS="-I$ROOTDIR/src/lib -I$ROOTDIR/src -I$ROOTDIR -I$ROOTDIR/.."
 
-cppcheck $ROOTDIR/src -I$ROOTDIR/src/lib -I$ROOTDIR/src -I$ROOTDIR --enable=$EIDS --quiet --error-exitcode=1 || exit 1
+cppcheck $ROOTDIR/src $INCLUDE_OPTS --enable=$EIDS --quiet --check-config --error-exitcode=1 || exit 1
+cppcheck $ROOTDIR/src $INCLUDE_OPTS --enable=$EIDS --quiet --error-exitcode=1 || exit 1
