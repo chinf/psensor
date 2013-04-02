883e54bb39729aaa9221bbebe9c9054ba99a3f5e
Jean-Philippe Orsini <jeanfi@gmail.com>
2013-04-02 08:39:41 +0000

    removed useless empty line

--- src/ui.h
+++ src/ui.h
@@ -97,4 +97,6 @@ void ui_cb_preferences(GtkMenuItem *mi, gpointer data);
 void ui_cb_about(GtkMenuItem *mi, gpointer data);
 void ui_cb_menu_quit(GtkMenuItem *mi, gpointer data);
 void ui_cb_sensor_preferences(GtkMenuItem *mi, gpointer data);
+
+struct psensor **ui_get_sensors_ordered_by_position(struct ui_psensor *);
 #endif
