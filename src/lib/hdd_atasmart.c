1ea7dfe115b5ae6f117636ad2d0ec366985b69f1
Jean-Philippe Orsini <jeanfi@gmail.com>
2013-10-07 08:53:10 +0000

    added LFS support

--- src/lib/hdd_atasmart.c
+++ src/lib/hdd_atasmart.c
@@ -16,6 +16,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA
  */
+#define _LARGEFILE_SOURCE 1
+#include "config.h"
+
 #include <locale.h>
 #include <libintl.h>
 #define _(str) gettext(str)
--- src/lib/pio.c
+++ src/lib/pio.c
@@ -16,6 +16,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301 USA
  */
+#define _LARGEFILE_SOURCE 1
+#include "config.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
