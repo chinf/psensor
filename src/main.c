7489cc10734e7ff49190f13337964d20489a480c
Jean-Philippe Orsini <jeanfi@gmail.com>
2014-06-01 00:26:54 +0200

    removed useless double whitespace in copyright.

--- src/main.c
+++ src/main.c
@@ -77,7 +77,7 @@ static void print_version()
 	printf(_("Copyright (C) %s jeanfi@gmail.com\n"
 		 "License GPLv2: GNU GPL version 2 or later "
 		 "<http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>\n"
-		 "This is free software: you are free to change and "
+		 "This is free software: you are free to change and"
 		 " redistribute it.\n"
 		 "There is NO WARRANTY, to the extent permitted by law.\n"),
 	       "2010-2014");
