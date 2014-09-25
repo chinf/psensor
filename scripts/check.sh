498aeeaef4fdde54833db90e88a370c8598d85e8
Jean-Philippe Orsini <jeanfi@gmail.com>
2014-09-25 10:06:22 +0200

    verify distrib build

--- scripts/check.sh
+++ scripts/check.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR/..
 
 ./configure --prefix=/tmp CC=cgcc CPPFLAGS="-Wsparse-all -DCURL_DISABLE_TYPECHECK -Wno-old-initializer"
-make clean all check install 3>&1 1>&2 2>&3 | grep -v "^/usr/include" | tee /tmp/err 
+make clean all check install distcheck 3>&1 1>&2 2>&3 | grep -v "^/usr/include" | tee /tmp/err 
 
 cat /tmp/err
 
