e56256536d00209b7cee3c7450d5e3fa50e3a966
Jean-Philippe Orsini <jeanfi@gmail.com>
2014-09-29 09:25:37 +0200

    stop on failures

--- scripts/check.sh
+++ scripts/check.sh
@@ -3,8 +3,9 @@
 SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR/..
 
-./configure --prefix=/tmp CC=cgcc CPPFLAGS="-Wsparse-all -DCURL_DISABLE_TYPECHECK -Wno-old-initializer"
-make clean all check install distcheck 3>&1 1>&2 2>&3 | grep -v "^/usr/include" | tee /tmp/err 
+./configure --prefix=/tmp CC=cgcc CPPFLAGS="-Wsparse-all -DCURL_DISABLE_TYPECHECK -Wno-old-initializer" || exit 1
+make clean || exit 1
+make all check install distcheck 3>&1 1>&2 2>&3 | grep -v "^/usr/include" | tee /tmp/err 
 
 cat /tmp/err
 
